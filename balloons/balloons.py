23d386878f682d2d1e53179dce011e7c42e03f14
aescudero <adrianescudero1305@gmail.com>
2023-11-07 09:25:22 -0600

    finished oop exercises

---
+++ balloons/balloons.py
@@ -0,0 +1,35 @@
+class BalloonTooFull(Exception):
+    pass
+
+
+class Balloon:
+    def __init__(self) -> None:
+        self.capacity = 0
+        self.amount = 0
+
+    def pump(self):
+        self.amount += 3
+        if self.amount > self.capacity:
+            raise BalloonTooFull("Pop!")
+
+    def release(self):
+        self.amount -= 2
+        self.amount = max(0, self.amount)
+
+
+class SwordBalloon(Balloon):
+    def __init__(self) -> None:
+        super().__init__()
+        self.capacity = 5
+
+
+class DogBalloon(Balloon):
+    def __init__(self) -> None:
+        super().__init__()
+        self.capacity = 7
+
+
+class TriforceBalloon(Balloon):
+    def __init__(self) -> None:
+        super().__init__()
+        self.capacity = 11
